8704d5c9877192749ec9a89643f670dc15ecb2d4
Agung Saputra <agungsaputradifh@gmail.com>
2023-08-31 16:01:47 +0700

    added fitur required regis

--- app/views/logister/register.php
+++ app/views/logister/register.php
@@ -3,25 +3,25 @@
         <form action="<?= BASEURL ?>/logister/register" method="post">
             <ul class="p-5">
                 <li class="py-1">
-                    <input type="text" name="username" placeholder="Username" class="w-full h-10 border-2 border-black rounded-lg px-3">
+                    <input type="text" name="username" placeholder="Username" required class="w-full h-10 border-2 border-black rounded-lg px-3">
                 </li>
                 <li class="py-1">
-                    <input type="password" name="password" placeholder="Password" class="w-full h-10 border-2 border-black rounded-lg px-3">
+                    <input type="password" name="password" placeholder="Password" required class="w-full h-10 border-2 border-black rounded-lg px-3">
                 </li>
                 <li class="py-1">
-                    <input type="password" name="password2" placeholder="Confirm Password" class="w-full h-10 border-2 border-black rounded-lg px-3">
+                    <input type="password" name="password2" placeholder="Confirm Password" required class="w-full h-10 border-2 border-black rounded-lg px-3">
                 </li>
                 <li class="py-1">
-                    <input type="text" name="notlp" placeholder="Nomer Telepon" class="w-full h-10 border-2 border-black rounded-lg px-3">
+                    <input type="text" name="notlp" placeholder="Nomer Telepon" required class="w-full h-10 border-2 border-black rounded-lg px-3">
                 </li>
                 <li class="py-1">
-                    <input type="text" name="nowa" placeholder="Nomer Whatsapp" class="w-full h-10 border-2 border-black rounded-lg px-3">
+                    <input type="text" name="nowa" placeholder="Nomer Whatsapp" required class="w-full h-10 border-2 border-black rounded-lg px-3">
                 </li>
                 <li class="py-1">
-                    <input type="text" name="address" placeholder="Alamat" class="w-full h-10 border-2 border-black rounded-lg px-3">
+                    <input type="text" name="address" placeholder="Alamat" required class="w-full h-10 border-2 border-black rounded-lg px-3">
                 </li>
                 <li class="py-1">
-                    <input type="text" name="email" placeholder="Email" class="w-full h-10 border-2 border-black rounded-lg px-3">
+                    <input type="text" name="email" placeholder="Email" required class="w-full h-10 border-2 border-black rounded-lg px-3">
                 </li>
                 <li class="py-1">Already have account? <a href="<?= BASEURL ?>/logister" class="text-red-500">login</a></li>
                 <li class="py-1">
