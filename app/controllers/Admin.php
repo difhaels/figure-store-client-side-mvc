4e7cb8768698a2d2aae671814ccced1f786e4f0f
Agung Saputra <agungsaputradifh@gmail.com>
2023-09-01 17:08:55 +0700

    atur controller admin

--- app/controllers/Admin.php
+++ app/controllers/Admin.php
@@ -4,6 +4,7 @@ class Admin extends Controller
 {
     public function index()
     {
+        // cek dulu admin sudah login atau belum
         if (isset($_SESSION['login-admin'])) {
             $data['nav'] = "back-button";
             $data['nav-short'] = "no";
@@ -31,10 +32,31 @@ class Admin extends Controller
         if ($this->model('Admin_model')->login()) {
             $_SESSION['login-admin'] = true;
 
-            // pindah ke dashboar admin
+            // pindah ke dashboard admin
             header("Location: " . BASEURL . "/admin");
             die;
         }
         echo "password atau username salah";
+    }
+
+    // method untuk admin
+    public function setting()
+    {
+        echo "admin";
+    }
+    // method untuk item
+    public function item()
+    {
+        echo "item";
+    }
+    // method untuk member
+    public function member()
+    {
+        echo "member";
+    }
+    // method untuk transaction
+    public function transaction()
+    {
+        echo "transaction";
     }
 }
--- app/views/admin/index.php
+++ app/views/admin/index.php
@@ -3,33 +3,33 @@
 
     <div class="flex justify-between items-center flex-wrap gap-3 pt-3 py-16">
 
-        <div class="w-full lg:w-80 bg-slate-200 cursor-pointer">
+        <a href="<?= BASEURL ?>/admin/setting" class="w-full lg:w-80 bg-slate-200 cursor-pointer">
             <div class="bg-red-500 pt-20 px-1 lg:px-3 pb-1 lg:pb-3">
                 <img src="<?= BASEURL ?>/img/icon/admin-a.png" alt="admin" class="w-10">
             </div>
             <h1 class="p-1 lg:p-3">Admin</h1>
-        </div>
+        </a>
 
-        <div class="w-full lg:w-80 bg-slate-200 cursor-pointer">
+        <a href="<?= BASEURL ?>/admin/item" class="w-full lg:w-80 bg-slate-200 cursor-pointer">
             <div class="bg-sky-500 pt-20 px-1 lg:px-3 pb-1 lg:pb-3">
                 <img src="<?= BASEURL ?>/img/icon/item-a.png" alt="item" class="w-10">
             </div>
             <h1 class="p-1 lg:p-3">Item</h1>
-        </div>
+        </a>
 
-        <div class="w-full lg:w-80 bg-slate-200 cursor-pointer">
+        <a href="<?= BASEURL ?>/admin/member" class="w-full lg:w-80 bg-slate-200 cursor-pointer">
             <div class="bg-green-500 pt-20 px-1 lg:px-3 pb-1 lg:pb-3">
                 <img src="<?= BASEURL ?>/img/icon/member-a.png" alt="member" class="w-10">
             </div>
             <h1 class="p-1 lg:p-3">Member</h1>
-        </div>
+        </a>
 
-        <div class="w-full lg:w-80 bg-slate-200 cursor-pointer">
+        <a href="<?= BASEURL ?>/admin/transaction" class="w-full lg:w-80 bg-slate-200 cursor-pointer">
             <div class="bg-yellow-200 pt-20 px-1 lg:px-3 pb-1 lg:pb-3">
                 <img src="<?= BASEURL ?>/img/icon/transaction-a.png" alt="transaction" class="w-10">
             </div>
             <h1 class="p-1 lg:p-3">Transaction</h1>
-        </div>
+        </a>
 
     </div>
 </div>
